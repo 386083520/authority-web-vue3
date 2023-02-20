d77b01a8a3640f1150c67fe5e751f89e4c86a3d5
gsd <386083520@qq.com>
2023-02-20 19:35:30 +0800

    引入vue-router;配置路径别名;快速引入element-plus;登陆页面内容居中

---
+++ src/views/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped lang="scss">
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
